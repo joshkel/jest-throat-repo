e654b07eef1166bb6a231db691787bc36d4c5a33
Josh Kelley <joshkel@gmail.com>
2022-04-27 08:46:01 -0400

    Initial commit

---
+++ jest.config.base.js
@@ -0,0 +1,6 @@
+const config = {
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  coverageReporters: ['text', 'lcov', 'cobertura'],
+};
+
+module.exports = config;
---
+++ jest.config.js
@@ -0,0 +1,5 @@
+const config = {
+  projects: ['<rootDir>/packages/*/jest.config.js'],
+};
+
+module.exports = config;
